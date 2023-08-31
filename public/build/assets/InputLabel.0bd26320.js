b5c900f7df3b3fe10a183e22b18498867e9cd824
ahmad <ahmad@starkleytech.com>
2023-09-01 03:21:07 +0500

    vite build files

---
+++ public/build/assets/InputLabel.0bd26320.js
@@ -0,0 +1,1 @@
+import{f as e,g as t,A as o,z as n}from"./app.e3c42c21.js";const l={class:"block font-medium text-sm text-gray-700 dark:text-gray-300"},r={key:0},c={key:1},i={__name:"InputLabel",props:["value"],setup(a){return(s,_)=>(e(),t("label",l,[a.value?(e(),t("span",r,o(a.value),1)):(e(),t("span",c,[n(s.$slots,"default")]))]))}};export{i as _};
