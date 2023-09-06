13862c65145bba891bd0ca41d17ee8b033d58186
ahmadrao <ahmadraosanawarali@gmail.com>
2023-09-07 01:11:39 +0500

    issues fixed

--- resources/js/Pages/Admin/Ratings/partials/SingleContractor/ReviewAdmin.vue
+++ resources/js/Pages/Admin/Ratings/partials/SingleContractor/ReviewAdmin.vue
@@ -2,7 +2,7 @@
   <div class="flex space-x-2 justify-between">
     <div class="flex justify-center items-center space-x-2">
       <div>
-        <Avatar :imageSrc="review.reviewer.user_avatar" />
+        <Avatar :imageSrc="`/${review.reviewer.user_avatar}`" />
       </div>
       <div class="flex flex-col justify-center">
         <Tooltip
@@ -247,7 +247,7 @@
       <div class="flex justify-between">
         <div class="flex items-center space-x-2">
           <div>
-            <Avatar :imageSrc="review.contractor.user_avatar" />
+            <Avatar :imageSrc="`/${review.contractor.user_avatar}`" />
           </div>
           <div class="flex flex-col justify-center">
             <Tooltip
