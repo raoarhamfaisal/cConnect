958ceae209195df7a083d35c827739714047a146
ahmadrao <ahmadraosanawarali@gmail.com>
2023-09-18 05:35:43 +0500

    code ready for deployment

--- public/build/assets/HeadingCard.f757cd24.js
+++
@@ -1,1 +0,0 @@
-import{a as e,c as a,x as n}from"./app.56c39d9a.js";const o={class:"font-bold text-xl text-blue-rgba leading-tight mb-5"},i={__name:"HeadingCard",props:["heading"],setup(t){return(s,r)=>(e(),a("div",o,n(t.heading),1))}};export{i as _};
