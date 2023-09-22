7dc86953c2d942dc0e373fed2101d0bb1861872f
ahmadrao <ahmadraosanawarali@gmail.com>
2023-09-22 11:31:34 +0500

    ready for deployment

---
+++ public/build/assets/HeadingCard.b2f08e17.js
@@ -0,0 +1,1 @@
+import{a as e,c as a,x as n}from"./app.ae2e3232.js";const o={class:"font-bold text-xl text-blue-rgba leading-tight mb-2"},i={__name:"HeadingCard",props:["heading"],setup(t){return(s,r)=>(e(),a("div",o,n(t.heading),1))}};export{i as _};
--- public/build/assets/HeadingCard.d4667a90.js
+++
@@ -1,1 +0,0 @@
-import{a as e,c as a,x as n}from"./app.73104a51.js";const o={class:"font-bold text-xl text-blue-rgba leading-tight mb-2"},i={__name:"HeadingCard",props:["heading"],setup(t){return(s,r)=>(e(),a("div",o,n(t.heading),1))}};export{i as _};
