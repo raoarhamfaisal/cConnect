83096535ff8c457934936a2de11c56cfc17fcec4
ahmadrao <ahmadraosanawarali@gmail.com>
2023-09-23 10:12:20 +0500

    ready for deploment

---
+++ public/build/assets/InputLabel.fa698c23.js
@@ -0,0 +1,1 @@
+import{a as e,c as t,x as o,k as n}from"./app.947025ce.js";const l={class:"block font-medium text-sm text-gray-700"},c={key:0},r={key:1},i={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[n(a.$slots,"default")]))]))}};export{i as _};
