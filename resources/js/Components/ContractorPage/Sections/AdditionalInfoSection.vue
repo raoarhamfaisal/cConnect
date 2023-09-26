cd27a142d5e080578aaa81ca6c2f9c5b0edc7166
ahmadrao <ahmadraosanawarali@gmail.com>
2023-09-26 11:33:13 +0500

    api binded

--- resources/js/Components/ContractorPage/Sections/AdditionalInfoSection.vue
+++ resources/js/Components/ContractorPage/Sections/AdditionalInfoSection.vue
@@ -251,7 +251,6 @@ import InfoWithIcon from "@/Components/ContractorPage/InfoWithIcon.vue";
 
 import Card from "@/Components/Card.vue";
 import UserAvatar from "@/Pages/Profile/components/UserAvatar.vue";
-import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import CustomDialog from "@/Components/Ratings/CustomDialog.vue";
