045f2fa58bcf32655b13f184d9e7034d1989f6e8
ahmadrao <ahmadraosanawarali@gmail.com>
2023-11-03 01:12:33 +0500

    autoprixer resolving

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-
+import autoprefixer from "autoprefixer";
 export default defineConfig({
   plugins: [
     laravel({
       input: "resources/js/app.js",
       refresh: true,
+      postcss: {
+        plugins: [autoprefixer()],
+      },
     }),
     vue({
       template: {
