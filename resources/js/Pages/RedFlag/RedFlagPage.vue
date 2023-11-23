1bdb274045e5d474a5aa32b4c3edc2b2b9a521aa
ahmadrao <ahmadraosanawarali@gmail.com>
2023-11-23 22:08:33 +0500

    red flag frontend design completed

--- resources/js/Pages/RedFlag/RedFlagPage.vue
+++ resources/js/Pages/RedFlag/RedFlagPage.vue
@@ -5,13 +5,16 @@ import { useStore } from "vuex";
 import { Icon } from "@iconify/vue";
 import MoveToTop from "@/Components/MoveToTop.vue";
 
-import { computed, nextTick, onMounted, ref, watch } from "vue";
+import { computed, nextTick, onMounted, reactive, ref, watch } from "vue";
 import SelectProfile from "@/Components/SelectProfile.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import Loader from "@/Components/Ratings/Loader.vue";
 import TableHead from "@/Pages/RedFlag/partials/TableHead.vue";
 import RedFlag from "@/Pages/RedFlag/partials/RedFlag.vue";
+
+import CustomDialog from "@/Components/Ratings/CustomDialog.vue";
+import Card from "@/Components/Card.vue";
 
 import { getAxiosConfig } from "@/helpers/axiosConfigHelpers";
 import { somethingWentWrong } from "@/helpers/utilities";
@@ -45,11 +48,11 @@ const redFlags = ref([
     },
 
     customerName: "George SuperIdolSmithy",
-    complaintSummary:
+    complaint:
       "I thought they did sub-par work work.  asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
   {
     id: 2,
@@ -60,11 +63,11 @@ const redFlags = ref([
     },
 
     customerName: "Rosealina Smithwhateverlast",
-    complaintSummary:
+    complaint:
       "Never paid the final insurance check. She told me that her insurance adjuster said that was her money so she took her family on a cruise. I asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
   {
     id: 3,
@@ -75,11 +78,11 @@ const redFlags = ref([
     },
 
     customerName: "George SuperIdolSmithy",
-    complaintSummary:
+    complaint:
       "I thought they did sub-par work work.  asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
   {
     id: 4,
@@ -90,11 +93,11 @@ const redFlags = ref([
     },
 
     customerName: "Rosealina Smithwhateverlast",
-    complaintSummary:
+    complaint:
       "Never paid the final insurance check. She told me that her insurance adjuster said that was her money so she took her family on a cruise. I asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
   {
     id: 5,
@@ -105,11 +108,11 @@ const redFlags = ref([
     },
 
     customerName: "George SuperIdolSmithy",
-    complaintSummary:
+    complaint:
       "I thought they did sub-par work work.  asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
   {
     id: 6,
@@ -120,11 +123,11 @@ const redFlags = ref([
     },
 
     customerName: "Rosealina Smithwhateverlast",
-    complaintSummary:
+    complaint:
       "Never paid the final insurance check. She told me that her insurance adjuster said that was her money so she took her family on a cruise. I asked told her that I need to file a lien on the house. Lien is filed but she still hasn't paid. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa delectus reprehenderit sit quaerat, veritatis facilis sapiente ab? Culpa deleniti voluptatibus ea nisi quibusdam eligendi, beatae, magnam reiciendis, sed omnis vel.",
     region: "1001 Dallas Ft Worth",
     dateReported: "12/30/23",
-    isCustomer: true,
+    isComplaintOnCustomer: true,
   },
 ]);
 
@@ -248,13 +251,138 @@ const fetchSearchedContractors = async (
     somethingWentWrong();
   }
 };
-
 const fetchSearchedContractorsWithLoading = async () => {
   loading.value = true;
   await fetchSearchedContractors();
   loading.value = false;
 };
 
+// Add new Red Flag and best practice open and close
+const addNewRedFlagDialogRef = ref();
+const bestPracticeDialogRef = ref();
+const doYouAgreeBestPracticeCheck = ref();
+const agreedBestPractice = ref(false);
+
+const openBestPracticeCard = () => {
+  if (!agreedBestPractice.value) {
+    doYouAgreeBestPracticeCheck.value = null;
+    bestPracticeDialogRef.value.openDialog();
+  } else {
+    addNewRedFlagDialogRef.value.openDialog();
+  }
+};
+
+watch(doYouAgreeBestPracticeCheck, (newValue) => {
+  if (newValue === false || newValue === true) {
+    setTimeout(() => {
+      bestPracticeDialogRef.value.closeDialog();
+    }, 100);
+  }
+  if (newValue) {
+    agreedBestPractice.value = true;
+    setTimeout(() => {
+      addNewRedFlagDialogRef.value.openDialog();
+    }, 100);
+  }
+});
+
+// add new Red Flag
+const newRedFlag = reactive({
+  customerName: "",
+  complaint: "",
+  isComplaintOnCustomer: null,
+
+  region_id: selectedObj ? selectedObj.id.toString() : undefined,
+});
+const errors = reactive({
+  customerName: null,
+  complaint: null,
+  isComplaintOnCustomer: null,
+  region_id: null,
+});
+const redFlagComplaintTextRef = ref();
+const selectedRegion = ref(selectedName ?? "");
+
+watch(
+  () => newRedFlag.isComplaintOnCustomer,
+  () => {
+    clearError("isComplaintOnCustomer");
+  }
+);
+
+const adjustHeight = () => {
+  nextTick(() => {
+    redFlagComplaintTextRef.value.style.height = "auto"; // Reset height first to get the correct scrollHeight
+    redFlagComplaintTextRef.value.style.height =
+      redFlagComplaintTextRef.value.scrollHeight + "px";
+  });
+};
+const changeRegion = (value) => {
+  selectedRegion.value = value;
+
+  props.regions.forEach((item) => {
+    if (value === item.name) {
+      newRedFlag.region_id = item.id.toString();
+    }
+  });
+  clearError("region_id");
+};
+const validateForm = () => {
+  let isValid = true;
+
+  // Reset errors
+  for (let field in errors) {
+    errors[field] = null;
+  }
+
+  // Validate customerName
+  if (!newRedFlag.customerName.trim()) {
+    errors.customerName = "Name is required.";
+    isValid = false;
+  }
+
+  // Validate complaint
+  if (!newRedFlag.complaint.trim()) {
+    errors.complaint = "Complaint/Review is required.";
+    isValid = false;
+  }
+
+  // Validate isComplaintOnCustomer
+  if (newRedFlag.isComplaintOnCustomer === null) {
+    errors.isComplaintOnCustomer =
+      "Please select if the complaint is on a customer or contractor.";
+    isValid = false;
+  }
+
+  // Validate region_id
+  if (!newRedFlag.region_id.trim()) {
+    errors.region_id = "Region is required.";
+    isValid = false;
+  }
+
+  return isValid;
+};
+const clearError = (field) => {
+  if (newRedFlag[field] !== null && field === "isComplaintOnCustomer") {
+    errors[field] = "";
+    return;
+  }
+  if (field === "isComplaintOnCustomer") {
+    return;
+  }
+  if (newRedFlag[field].trim()) {
+    errors[field] = "";
+  }
+};
+
+const onAddNewRedFlag = () => {
+  if (validateForm()) {
+    console.log("newRedFlag", newRedFlag.value);
+    addNewRedFlagDialogRef.value.closeDialog();
+  }
+};
+
+// for border visibility of between two red flags
 const openAccordions = ref([]);
 
 const handleAccordionToggle = ({ index, isOpen }) => {
@@ -320,6 +448,7 @@ const determineBorderVisibility = (index) => {
             <button
               type="button"
               class="w-4/5 px-1 py-1 h-[48px] rounded-lg transition transform duration-300 hover:shadow-lg active:scale-95 font-extrabold bg-[#e80000] text-white text-md sm:text-xl font-sans"
+              @click="openBestPracticeCard"
             >
               Add New Red Flag
             </button>
@@ -344,7 +473,8 @@ const determineBorderVisibility = (index) => {
                     v-model="isSearchByCustomer"
                     color="error"
                     id="isSearchByCustomer"
-                    :value="true"
+                    :true-value="true"
+                    :false-value="null"
                     style="font-size: 1.5rem"
                     hide-details
                   >
@@ -352,7 +482,7 @@ const determineBorderVisibility = (index) => {
                   <div
                     for="isSearchByCustomer"
                     @click="isSearchByCustomer = true"
-                    class="w-full cursor-pointer text-sm sm:text-xl translate-y-[0px]"
+                    class="w-full cursor-pointer text-sm sm:text-xl"
                   >
                     Customer
                   </div>
@@ -363,14 +493,15 @@ const determineBorderVisibility = (index) => {
                     color="error"
                     id="notSearchByCustomer"
                     style="font-size: 1.5rem"
-                    :value="false"
+                    :true-value="false"
+                    :false-value="null"
                     hide-details
                   >
                   </v-checkbox>
                   <div
                     for="notSearchByCustomer"
                     @click="isSearchByCustomer = false"
-                    class="w-full cursor-pointer text-sm sm:text-xl translate-y-[0px]"
+                    class="w-full cursor-pointer text-sm sm:text-xl"
                   >
                     Contractor(non-member)
                   </div>
@@ -475,6 +606,183 @@ const determineBorderVisibility = (index) => {
       </div>
     </div>
 
+    <!-- best practice Dialog -->
+    <CustomDialog
+      :showHeader="false"
+      dialogWidth="card-width bg-[#f0f4f7] p-2 "
+      :showFooter="false"
+      ref="bestPracticeDialogRef"
+    >
+      <section class="card-content">
+        <p class="mt-1 mb-2 text-xl font-bold">Best Practice For Reviewers:</p>
+        <p class="text-lg">
+          While you have every right to express your opinion, choose what you
+          write carefully to avoid any Defamation or libel. Ensure what you
+          write is only the truth. Avoid writing reviews in a way that can be
+          construed as falsehoods. Only write about what can be backed up by
+          evidence.
+        </p>
+        <p class="mt-4 mb-4 text-lg">
+          As long as a review is based on facts and a personal point of view, it
+          can be less vulnerable to argument.
+        </p>
+      </section>
+      <!-- agree disagree -->
+      <div class="card-footer">
+        <div class="w-full -translate-x-2 flex gap-1">
+          <div class="flex gap-4 items-center h-8">
+            <v-checkbox
+              v-model="doYouAgreeBestPracticeCheck"
+              color="primary"
+              id="doYouAgreeBestPracticeCheck"
+              :true-value="true"
+              :false-value="null"
+              style="font-size: 1.3rem"
+              hide-details
+            >
+            </v-checkbox>
+            <div
+              for="doYouAgreeBestPracticeCheck"
+              @click="doYouAgreeBestPracticeCheck = true"
+              class="w-full cursor-pointer text-sm sm:text-xl"
+            >
+              I Accept
+            </div>
+          </div>
+          <div class="flex gap-4 items-center h-8">
+            <v-checkbox
+              v-model="doYouAgreeBestPracticeCheck"
+              color="primary"
+              id="notAgreeBestPractice"
+              style="font-size: 1.3rem"
+              :true-value="false"
+              :false-value="null"
+              hide-details
+            >
+            </v-checkbox>
+            <div
+              for="notAgreeBestPractice"
+              @click="doYouAgreeBestPracticeCheck = false"
+              class="w-full cursor-pointer text-sm sm:text-xl"
+            >
+              Do not Agree, Exit
+            </div>
+          </div>
+        </div>
+      </div>
+    </CustomDialog>
+
+    <!-- Add New Red Flag Dialog -->
+    <CustomDialog
+      :showHeader="false"
+      dialogWidth="card-width bg-[#fff] p-2 "
+      :showFooter="false"
+      ref="addNewRedFlagDialogRef"
+    >
+      <section class="card-content">
+        <div
+          class="w-[46px]"
+          @click="() => addNewRedFlagDialogRef.closeDialog()"
+        >
+          <Icon
+            :icon="'emojione-monotone:cross-mark'"
+            color="#e80000"
+            class="w-7 h-7 transition-transform duration-500 cursor-pointer"
+          />
+        </div>
+        <div
+          class="relative mb-2 mt-4 flex w-full sm:w-1/2 flex-wrap items-stretch transition duration-300 ease-in-out"
+        >
+          <input
+            type="text"
+            id="search"
+            v-model="newRedFlag.customerName"
+            class="relative m-0 flex-grow block w-full h-[45px] px-3 py-1.5 text-base font-normal text-gray-700 outline-none rounded-md border-solid border-gray-600 placeholder:italic placeholder:text-slate-500 pr-12"
+            placeholder="Enter Name..."
+            @input="clearError('customerName')"
+          />
+          <InputError class="mt-2" :message="errors.customerName" />
+        </div>
+        <textarea
+          ref="redFlagComplaintTextRef"
+          v-model="newRedFlag.complaint"
+          @paste="adjustHeight"
+          @input="adjustHeight"
+          @keyup="clearError('complaint')"
+          placeholder="Write complaint/review here!"
+          class="text-sm w-full py-2 min-h-[100px] placeholder:text-base overflow-y-hidden px-3 border-solid border-gray-600 sm:mt-2 bg-white border-gray-200 text-grey-600 resize-none rounded"
+        ></textarea>
+        <InputError class="mt-2" :message="errors.complaint" />
+
+        <div class="w-full flex flex-col gap-1 -translate-x-2 mt-1 sm:mt-2">
+          <div class="flex gap-2 items-center h-8">
+            <v-checkbox
+              v-model="newRedFlag.isComplaintOnCustomer"
+              color="#241e6d"
+              id="newRedFlag.isComplaintOnCustomer"
+              :true-value="true"
+              :false-value="null"
+              style="font-size: 1.3rem"
+              hide-details
+            >
+            </v-checkbox>
+            <div
+              for="newRedFlag.isComplaintOnCustomer"
+              @click="newRedFlag.isComplaintOnCustomer = true"
+              class="w-full cursor-pointer text-sm sm:text-xl"
+            >
+              Customer
+            </div>
+          </div>
+          <div class="flex gap-2 items-center h-8">
+            <v-checkbox
+              v-model="newRedFlag.isComplaintOnCustomer"
+              color="#241e6d"
+              id="notisComplaintOnCustomer"
+              style="font-size: 1.3rem"
+              :true-value="false"
+              :false-value="null"
+              hide-details
+            >
+            </v-checkbox>
+            <div
+              for="notisComplaintOnCustomer"
+              @click="newRedFlag.isComplaintOnCustomer = false"
+              class="w-full cursor-pointer text-sm sm:text-xl"
+            >
+              Contractor(non-member)
+            </div>
+          </div>
+        </div>
+        <InputError class="mt-2" :message="errors.isComplaintOnCustomer" />
+
+        <!-- seelct region -->
+        <div class="w-full sm:w-1/2 my-2">
+          <div class="">
+            <InputLabel
+              class="font-bold text-base mb-1"
+              value="Select Region:"
+            />
+            <SelectProfile
+              class="bg-white rounded"
+              :options="referenceList"
+              :modelValue="selectedRegion"
+              @update:modelValue="changeRegion"
+            />
+            <InputError class="mt-2" :message="errors.region_id" />
+          </div>
+        </div>
+        <!-- Submit -->
+        <button
+          @click="onAddNewRedFlag"
+          class="border-2 mt-5 w-full sm:w-40 flex items-center justify-center border-2 border-teal-green bg-teal-green text-white font-semibold text-xl py-2 px-4 rounded transition transform duration-300 hover:shadow-lg active:scale-95"
+        >
+          Save
+        </button>
+      </section>
+
+      <!-- agree disagree -->
+    </CustomDialog>
     <!-- search results -->
     <MoveToTop />
   </Header>
