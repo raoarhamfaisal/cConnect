b94c12d93e4143c96b4f1ea4cca5997676c5daf5
ahmadrao <ahmadraosanawarali@gmail.com>
2023-12-02 17:47:32 +0500

    payment related changes

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -739,7 +739,7 @@ const formatPrice = (price) => {
           <div
             class="mx-auto text-xs mb-6 text-center first-letter: text-white md:hidden"
           >
-            0062311300410
+            0082311301540
           </div>
           <div
             class="mx-auto my-6 text-center first-letter: text-white md:hidden"
@@ -882,7 +882,7 @@ const formatPrice = (price) => {
           <div
             class="hidden text-xs text-white md:block my-2 text-center first-letter:"
           >
-          0062311300410
+          0082311301540
           </div>
         </div>
       </div>
