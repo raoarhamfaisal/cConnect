ddee77271d3f7b04e3b546e13f78e8f00856d305
ahmad <ahmad@starkleytech.com>
2024-01-26 17:08:38 +0500

    review response active, deactivate and also get the contractor details for admin

--- routes/api.php
+++ routes/api.php
@@ -38,10 +38,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::delete('/review-responses/{reviewResponse}', [ReviewResponseController::class, 'destroy']);
 
     Route::get('/admin/all-contractors', [ReviewController::class, 'getContractorProfiles'])->name('review.allContractors');
+    Route::get('/admin/reviews/{contractor_id}', [ReviewController::class, 'contractorAllReviews'])->name('review.contractorAllReviews');
+
 
     Route::get('/admin/all-appealed-reviews', [ReviewController::class, 'getAppealedReviews'])->name('review.getAppealedReviews');
     Route::put('/admin/reviews/{review}/deactivate', [ReviewController::class, 'deactivate']);
     Route::put('/admin/reviews/{review}/activate', [ReviewController::class, 'activate']);
+
+    Route::put('/admin/review-responses/{reviewResponse}/deactivate', [ReviewResponseController::class, 'deactivate']);
+    Route::put('/admin/review-responses/{reviewResponse}/activate', [ReviewResponseController::class, 'activate']);
 
 
 // });
