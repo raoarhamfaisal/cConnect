a471c11bc0345047c593a809df0abb4ff7f57ac2
ahmad <ahmad@starkleytech.com>
2024-01-28 13:27:19 +0500

    contractor details in reviews response

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -375,6 +375,50 @@ class ReviewController extends Controller
                 'trade29',
                 'trade30'
             ]);
+        }, 'contractor' => function($query) {
+            $query->select([
+                'id',
+                'user_id',
+                'email',
+                'phone_cell',
+                'first_name',
+                'last_name',
+                'company_name',
+                'city',
+                'state',
+                'user_avatar',
+                'company_logo',
+                'trade1',
+                'trade2',
+                'trade3',
+                'trade4',
+                'trade5',
+                'trade6',
+                'trade7',
+                'trade8',
+                'trade9',
+                'trade10',
+                'trade11',
+                'trade12',
+                'trade13',
+                'trade14',
+                'trade15',
+                'trade16',
+                'trade17',
+                'trade18',
+                'trade19',
+                'trade20',
+                'trade21',
+                'trade22',
+                'trade23',
+                'trade24',
+                'trade25',
+                'trade26',
+                'trade27',
+                'trade28',
+                'trade29',
+                'trade30'
+            ]);
         }, 'review_response'])->where('is_under_appeal', 1);
     
         // Apply sorting based on filters
