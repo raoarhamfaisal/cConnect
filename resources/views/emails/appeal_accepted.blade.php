d609cb4297ecadd24051216be9a9b24216a5f4a9
ahmad <ahmad@starkleytech.com>
2024-01-29 21:34:56 +0500

    1. Put condition for 48 hours. Make the reviewer submit a review to a contractor only once in 48 hours.

---
+++ resources/views/emails/appeal_accepted.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Appeal Accepted</title>
+</head>
+<body>
+    <h1>Your appeal has been accepted.</h1>
+    <p>Your request to change the review has been accepted. Thank you for bringing it to our attention.</p>
+</body>
+</html>
---
+++ resources/views/emails/appeal_rejected.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Appeal Rejected</title>
+</head>
+<body>
+    <h1>Your Appeal Has Been Rejected</h1>
+    <p>We regret to inform you that your request to change the review has been rejected after careful consideration.</p>
+    <p>If you have any questions or need further clarification, please feel free to contact us.</p>
+</body>
+</html>
