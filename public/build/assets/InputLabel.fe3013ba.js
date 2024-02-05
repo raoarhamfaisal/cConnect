2fd308e42eab434607e279b0e874468630ae4c68
ahmadrao <ahmadraosanawarali@gmail.com>
2024-02-06 04:12:41 +0500

    changes regarding deployment

--- public/build/assets/InputLabel.fe3013ba.js
+++
@@ -1,1 +0,0 @@
-import{o as e,a as t,t as o,r as n}from"./app.1440a0ce.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},i={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[n(a.$slots,"default")]))]))}};export{i as _};
