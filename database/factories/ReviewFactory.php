dc2935f6ab15990f35a15ed16e52976f01c16e51
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-02-15 00:32:46 +0500

    review ffactory changes

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -20,6 +20,25 @@ class ReviewFactory extends Factory
      */
     public function definition()
     {
+        $appeal_status = $this->faker->randomElement(['open', 'on_hold', 'approved', 'denied']);
+
+        $is_review_active = 1; 
+        $is_under_appeal = 1;
+
+        if($appeal_status === "open") {
+            $is_review_active = 1;
+            $is_under_appeal = 1;
+        }else if($appeal_status === "on_hold") {
+            $is_review_active = 1;
+            $is_under_appeal = 1;
+        }else if($appeal_status === "approved") {
+            $is_review_active = 0;
+            $is_under_appeal = 0;
+        }else if($appeal_status === "denied") {
+            $is_review_active = 1;
+            $is_under_appeal = 0;
+        }
+
         return [
             'reviewer_id' => Profile::inRandomOrder()->first()->id,
             'contractor_id' => Profile::inRandomOrder()->first()->id,
@@ -29,16 +48,15 @@ class ReviewFactory extends Factory
             'rating_date' => $this->faker->dateTime($max = 'now'),
             'on_appeal_reason' => $this->faker->text(),
             'on_appeal_reason_date' => $this->faker->dateTime($max = 'now'),
-            'off_appeal_reason' => $this->faker->text(),
-            'appeal_status' => $this->faker->randomElement(['open', 'on_hold', 'approved', 'denied']),
-            'off_appeal_reason_date' => $this->faker->dateTime($max = 'now'),
-            'is_under_appeal' => $this->faker->numberBetween(0, 1),
+            'appeal_status' => $appeal_status,
+            'is_review_active' => $is_review_active,
+            'is_under_appeal' => $is_under_appeal,
             'hired_by_contractor' => $this->faker->numberBetween(0, 1),
             'paid_on_time' => $this->faker->numberBetween(0, 1),
             'hired_contractor' => $this->faker->numberBetween(0, 1),
             'give_full_payment' => $this->faker->numberBetween(0, 1),
             'how_did_you_meet_this_contractor' => 'tContractor Referral'
         ];
-
     }
 }
+
