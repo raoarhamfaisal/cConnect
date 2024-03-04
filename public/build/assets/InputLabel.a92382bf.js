71d4fdc4d5e469fffcbb69945830c635be58f0ba
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-03-04 22:39:06 +0500

    ready for deployment

---
+++ public/build/assets/InputLabel.a92382bf.js
@@ -0,0 +1,1 @@
+import{a as e,c as t,A as o,p as n}from"./app.85505c6c.js";const l={class:"block font-medium text-sm text-gray-700"},c={key:0},r={key:1},u={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[n(a.$slots,"default")]))]))}};export{u as _};
