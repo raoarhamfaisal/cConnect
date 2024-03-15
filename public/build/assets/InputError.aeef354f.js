3ad4a50b9447842a0ae67012a7753c0488633525
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-03-15 23:41:50 +0500

    ready for deployment

---
+++ public/build/assets/InputError.aeef354f.js
@@ -0,0 +1,1 @@
+import{J as s,O as t,a,c as r,k as c,A as o}from"./app.0d400f46.js";const n={class:"text-sm text-red-600"},p={__name:"InputError",props:["message"],setup(e){return(i,m)=>s((a(),r("div",null,[c("p",n,o(e.message),1)],512)),[[t,e.message]])}};export{p as _};
