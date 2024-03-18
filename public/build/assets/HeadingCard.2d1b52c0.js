9b230fb643663cff1671725e7e9e82a32476d705
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-03-18 16:53:15 +0500

    contractor, profile update issue resolved

--- public/build/assets/HeadingCard.2d1b52c0.js
+++
@@ -1,1 +0,0 @@
-import{a as e,c as a,A as n}from"./app.0d400f46.js";const o={class:"font-bold text-xl text-blue-rgba leading-tight mb-2"},i={__name:"HeadingCard",props:["heading"],setup(t){return(s,r)=>(e(),a("div",o,n(t.heading),1))}};export{i as _};
