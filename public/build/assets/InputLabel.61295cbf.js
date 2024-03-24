b7c2e76c52e58d9e04cc854b72de2c142d724e72
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-03-24 13:52:29 +0500

    payment cancel issue fixed

--- public/build/assets/InputLabel.61295cbf.js
+++
@@ -1,1 +0,0 @@
-import{g as e,b as t,x as o,v as n}from"./app.ffd4b433.js";const l={class:"block font-medium text-sm text-gray-700"},c={key:0},r={key:1},i={__name:"InputLabel",props:["value"],setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[n(a.$slots,"default")]))]))}};export{i as _};
